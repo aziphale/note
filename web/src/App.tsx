5fb85aa0fbfc1728c33d5c67bb9c8eefccbf91ef
sheffery <sheffery@outlook.com>
2022-04-22 17:48:23 +0800

    feat: full screen mode, bind F9 and F10

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
-import React, {ChangeEvent, KeyboardEventHandler, useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
-import Blank from "./part/Blank";
 import transfer from "./utils/markdown";
 import refresh from "./utils/highlight";
+
+const INDENT = '    ';
+
+const enum SHOW_MODE {
+    HALF,
+    EDIT,
+    SHOW
+}
 
 function App() {
 
@@ -30,9 +37,10 @@ function App() {
     useEffect(() => {
         console.log("refresh");
         refresh();
-    })
+    }, [content]);
 
     let onKeyPress: React.KeyboardEventHandler<HTMLTextAreaElement> = (event) => {
+        let markdown: HTMLTextAreaElement = event.target as HTMLTextAreaElement;
         if (event.code === "Enter") {
             fetch("api/update", {
                 headers: {
@@ -48,17 +56,58 @@ function App() {
                 }
             });
         }
+        if (event.code === "Tab") {
+            event.preventDefault();
+            let start = markdown.selectionStart;
+            let end = markdown.selectionEnd;
+            markdown.value = markdown.value.substring(0,start) + INDENT + markdown.value.substring(end);
+        }
     }
 
     let onChange: React.ChangeEventHandler<HTMLTextAreaElement> = (event: ChangeEvent<HTMLTextAreaElement>) => {
         setContent(event.target.value);
     }
 
+    const [mode, setMode] = useState(SHOW_MODE.HALF);
+
+    useEffect(() => {
+        // @ts-ignore
+        let viewChange = event => {
+            if (event.code === "F11") {
+                event.preventDefault();
+                console.log("half view");
+                setMode(SHOW_MODE.HALF);
+            }
+            if (event.code === "F10") {
+                event.preventDefault();
+                console.log("show view");
+                setMode(SHOW_MODE.SHOW);
+            }
+            if (event.code === "F9") {
+                event.preventDefault();
+                console.log("edit view");
+                setMode(SHOW_MODE.EDIT);
+            }
+        }
+        addEventListener("keydown", viewChange);
+        return () => {
+            removeEventListener("keydown", viewChange);
+        }
+    }, [mode]);
+
+    console.log("view init")
+
     return (
         <div className="App">
-            <textarea id='note-md-id' defaultValue={content} onChange={onChange} onKeyDown={onKeyPress}></textarea>
-            <Blank/>
-            <div id='note-html-id' dangerouslySetInnerHTML={transfer(content)}></div>
+            <textarea id='note-md-id' className={SHOW_MODE.HALF === mode
+                ? "half" : (SHOW_MODE.EDIT === mode ? "full" : "hide")}
+                      defaultValue={content}
+                      onChange={onChange}
+                      onKeyDown={onKeyPress}></textarea>
+            <div id='note-blank-id' className={SHOW_MODE.HALF === mode ? "blank" : "hide"}></div>
+            <div id='note-html-id' className={SHOW_MODE.HALF === mode
+                ? "half" : (SHOW_MODE.SHOW === mode ? "full" : "hide")}
+                 dangerouslySetInnerHTML={transfer(content)}></div>
         </div>
     );
 }
